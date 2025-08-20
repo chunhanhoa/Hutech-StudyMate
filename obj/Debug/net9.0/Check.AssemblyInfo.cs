8349f8f136a532b4fe320193aed16ea3bbf5f8e6
chunhanhoa <hoachunhan@gmail.com>
2025-08-20 16:26:15 +0700

    Update interface

--- obj/Debug/net9.0/Check.AssemblyInfo.cs
+++ obj/Debug/net9.0/Check.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Check")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44babfa5c9146da65b82fabaa4569dbca904274b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23b600496754c3b427c6be989c364d8cbff2742")]
 [assembly: System.Reflection.AssemblyProductAttribute("Check")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Check")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
