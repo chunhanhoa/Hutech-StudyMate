a6e66ac772ffdf4603fe6942ef254bfb53e9add2
chunhanhoa <hoachunhan@gmail.com>
2025-09-02 23:41:00 +0700

    Update AI 3D

--- obj/Debug/net9.0/Check.AssemblyInfo.cs
+++ obj/Debug/net9.0/Check.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Check")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ea75eecfc8d28a81351ca976389220b4889aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ac41b245d60795ab7925cd5f16f7c36f56b6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Check")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Check")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
